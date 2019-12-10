db3e9c9f5954fcf7789bab617994c5483467dc12
MoonYeeun <aams82@hanmail.net>
2019-12-10 23:19:13 +0900

    이친수

--- src/boj/Back_2193.java
+++ src/boj/Back_2193.java
@@ -1,19 +1,17 @@
 package boj;
 
 import java.util.Scanner;
-//백준
+//백준 이친수
 public class Back_2193 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        long[] arr = new long[n+1];
-        arr[0] = 0;
-        arr[1] = 1;
-        arr[2] = 2;
-        for(int i = 3; i <= n ; i++){
-            arr[i] = arr[i-1] + arr[i-2];
-            arr[i] %= 15746;
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            long[] arr = new long[n+1];
+            arr[0] = 0;
+            arr[1] = 1;
+            for(int i = 2; i <= n ; i++){
+                arr[i] = arr[i-1] + arr[i-2];
+            }
+            System.out.println(arr[n]);
         }
-        System.out.println(arr[n]);
-    }
 }
