4863ef66bca1a0016ed6ca5db4a9db709e073638
MoonYeeun <aams82@hanmail.net>
2020-03-05 19:03:40 +0900

    2020 카카오 괄호변환 (지저분한 코드 수정)

--- src/programmers/Programmers_translate_bracket.java
+++ src/programmers/Programmers_translate_bracket.java
@@ -13,16 +13,17 @@ public class Programmers_translate_bracket {
         String u = "";
         String v = "";
         String answer = "";
+        // 빈 문자열인 경우
         if(str.isEmpty())
             return str;
-        char past_str = str.charAt(0);// 이전 문자열을 기억할 변수
+
+        char past_str = str.charAt(0); // 이전 문자열을 기억할 변수
         Stack<Character> stack = new Stack<>();
         stack.add(str.charAt(0));
         for(int i = 1 ; i < len ; i++) {
             if (!stack.isEmpty() && past_str != str.charAt(i)) {
                 stack.pop();
-                if(str.charAt(i) == '(') past_str = ')';
-                else past_str = '(';
+                past_str = (str.charAt(i) == '(') ? ')' : '(';
             } else if (past_str == str.charAt(i))
                 stack.add(str.charAt(i));
 
@@ -32,27 +33,25 @@ public class Programmers_translate_bracket {
                 break;
             }
         }
-        // 올바른 문자열인지 체크
+
         if(goodStr_Check(u)) {
             answer += u + strCheck(v);
             return answer;
         } else {
-            String temp = "(" + strCheck(v) + ")"; // u의 첫 번째와 마지막 문자를 제거
-            if(u.length() > 2) {
-                u = u.substring(1,u.length()-1);
-                StringBuilder sb = new StringBuilder(u);
-                for(int i = 0 ; i < u.length() ; i++) {
-                    if(u.charAt(i) == '('){
-                        sb.setCharAt(i, ')');
-                        u = sb.toString();
-                    }
-                    else {
-                        sb.setCharAt(i, '(');
-                        u = sb.toString();
-                    }
+            String temp = "(" + strCheck(v) + ")"; // 4-1 ~ 4-3 과정
+            u = u.substring(1,u.length()-1); // u의 첫 번째와 마지막 문자를 제거
+
+            StringBuilder sb = new StringBuilder(u);
+            for(int i = 0 ; i < u.length() ; i++) {
+                if(u.charAt(i) == '('){
+                    sb.setCharAt(i, ')');
+                    u = sb.toString();
+                }
+                else {
+                    sb.setCharAt(i, '(');
+                    u = sb.toString();
                 }
             }
-            else u = "";
             answer += temp + u;
             return answer;
         }
@@ -61,7 +60,7 @@ public class Programmers_translate_bracket {
     static boolean goodStr_Check(String str) {
         Stack<Character> check = new Stack<>();
         int str_len = str.length();
-        for(int i = 0 ; i < str_len ; i++){
+        for(int i = 0 ; i < str_len ; i++) {
             if(str.charAt(i) == '(')
                 check.add(str.charAt(i));
             if(check.isEmpty() && str.charAt(i) == ')')
