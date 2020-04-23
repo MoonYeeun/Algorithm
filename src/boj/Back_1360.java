e5933da0138eb3bad7254c621a4478c05f203257
MoonYeeun <aams82@hanmail.net>
2020-04-24 00:29:34 +0900

    되돌리기

--- src/boj/Back_1360.java
+++ src/boj/Back_1360.java
@@ -3,7 +3,11 @@ package boj;
 import java.util.*;
 
 // 백준 1360 되돌리기...
-
+/* 스택에서 하나씩 빼면서
+undo의 경우 (현재시간 - 되돌릴 시간) 보다 큰 시간은 무시해주면서
+답 찾아가면 된다
+(최종 undo 만이 효력있으므로 그 전 값들 무시 가능)
+ */
 public class Back_1360 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
