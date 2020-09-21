bc2f35a60faea1743e3e92efa3305bbf37faed0f
MoonYeeun <aams82@hanmail.net>
2020-09-21 23:59:36 +0900

    개미

---
+++ src/boj/Back_2136.java
@@ -0,0 +1,11 @@
+package boj;
+
+import java.util.*;
+
+public class Back_2136 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int len = sc.nextInt();
+        int n = sc.nextInt();
+    }
+}
