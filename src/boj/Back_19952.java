dbaf905ffb1e926fe99b5e6f91dbf582a294bb2a
MoonYeeun <aams82@hanmail.net>
2020-10-12 23:50:12 +0900

    인성 문제 있어?

---
+++ src/boj/Back_19952.java
@@ -0,0 +1,24 @@
+package boj;
+
+import java.util.*;
+
+// 백준 19952 인성 문제 있어?
+public class Back_19952 {
+    static int w, h, o, force, startX, startY, desX, desY;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            w = sc.nextInt();
+            h = sc.nextInt();
+            o = sc.nextInt();
+            force = sc.nextInt();
+            startX = sc.nextInt();
+            startY = sc.nextInt();
+            desX = sc.nextInt();
+            desY = sc.nextInt();
+        }
+    }
+}
