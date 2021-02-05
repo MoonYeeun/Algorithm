90eda3a32a39e3fbe1e7bb000d3ee4ce6ba0f54b
MoonYeeun <aams82@hanmail.net>
2021-02-06 00:01:00 +0900

    986. Interval List Intersections (시간 줄인 코드 추가)

--- src/LeetCode/LC986_Interval_List_Intersections.java
+++ src/LeetCode/LC986_Interval_List_Intersections.java
@@ -14,48 +14,86 @@ public class LC986_Interval_List_Intersections {
         }
     }
 
+    // 처음 짠 코드
+//    public static int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
+//        int[][] ans = {};
+//
+//        if (firstList.length == 0 || secondList.length == 0) return ans;
+//
+//        ArrayList<int[]> result = new ArrayList<>();
+//        ArrayList<int[]> list = new ArrayList<>();
+//
+//        // 두 개의 배열 하나로 합친 후 정렬
+//        list.addAll(arrToList(firstList));
+//        list.addAll(arrToList(secondList));
+//
+//        Collections.sort(list, new Comparator<int[]>() {
+//            @Override
+//            public int compare(int[] p1, int[] p2) {
+//                if (p1[0] == p2[0]) return p1[1] - p2[1];
+//                return p1[0] - p2[0];
+//            }
+//        });
+//
+//        // 겹치는 부분 계산
+//        int e = -1;
+//        for (int[] cur : list) {
+//            if (cur[1] <= e) {
+//                result.add(cur);
+//                continue;
+//            } else if (cur[0] <= e) {
+//                int[] newArr = {cur[0], e};
+//                result.add(newArr);
+//            }
+//
+//            e = cur[1];
+//        }
+//        return result.toArray(new int[0][]);
+//    }
+//
+//    static ArrayList<int[]> arrToList(int[][] list) {
+//        ArrayList<int[]> result = new ArrayList<>();
+//
+//        for (int[] cur : list) {
+//            result.add(cur);
+//        }
+//        return result;
+//    }
+    // 시간 줄인 코드
     public static int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
         int[][] ans = {};
 
         if (firstList.length == 0 || secondList.length == 0) return ans;
 
-        ArrayList<int[]> result = new ArrayList<>();
-        ArrayList<int[]> list = new ArrayList<>();
+        int aLen = firstList.length;
+        int bLen = secondList.length;
+        int aIdx = 0;
+        int bIdx = 0;
 
-        // 두 개의 배열 하나로 합친 후 정렬
-        list.addAll(arrToList(firstList));
-        list.addAll(arrToList(secondList));
+        List<int[]> results = new ArrayList<>();
 
-        Collections.sort(list, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] p1, int[] p2) {
-                if (p1[0] == p2[0]) return p1[1] - p2[1];
-                return p1[0] - p2[0];
-            }
-        });
-
-        // 겹치는 부분 계산
-        int e = -1;
-        for (int[] cur : list) {
-            if (cur[1] <= e) {
-                result.add(cur);
-                continue;
-            } else if (cur[0] <= e) {
-                int[] newArr = {cur[0], e};
-                result.add(newArr);
+        while (aIdx < aLen && bIdx < bLen) {
+            // 두 부분 중 겹치는 부분 계산
+            int[] res = calculate(firstList[aIdx], secondList[bIdx]);
+            if (res.length > 0) {
+                results.add(res);
             }
 
-            e = cur[1];
+            if (firstList[aIdx][1] > secondList[bIdx][1]) bIdx++;
+            else aIdx++;
         }
-        return result.toArray(new int[0][]);
+
+        return results.toArray(new int[0][]);
     }
 
-    static ArrayList<int[]> arrToList(int[][] list) {
-        ArrayList<int[]> result = new ArrayList<>();
+    static int[] calculate(int[] a, int[] b) {
+        int start = Math.max(a[0], b[0]);
+        int end = Math.min(a[1], b[1]);
 
-        for (int[] cur : list) {
-            result.add(cur);
+        // 겹치는 부분 없는 경우
+        if (start > end) {
+            return new int[0];
         }
-        return result;
+        return new int[]{start, end};
     }
 }
