bb42d0c97af529ac447a6a06df7456177272d092
MoonYeeun <aams82@hanmail.net>
2021-02-17 01:25:18 +0900

    838. Push Dominoes

--- src/LeetCode/LC838_Push_Dominoes.java
+++ src/LeetCode/LC838_Push_Dominoes.java
@@ -1,22 +1,50 @@
 package LeetCode;
 
-import java.util.*;
-
+// 투포인터
 public class LC838_Push_Dominoes {
     public static void main(String[] args) {
-        String dominoes = ".L.R...LR..L..";
+        //String dominoes = ".L.R...LR..L..";
+        String dominoes = "RR.L";
         System.out.println(pushDominoes(dominoes));
     }
 
     public static String pushDominoes(String dominoes) {
+        char[] result = dominoes.toCharArray();
         int len = dominoes.length();
-        char[] result = new char[len];
-        Arrays.fill(result, '.');
+        int left = 0;
+        int right = 0;
 
-        StringBuilder sb = new StringBuilder();
-        for (char c : result) {
-            sb.append(c);
+        while (left < len) {
+            while (right < len && result[right] == '.') {
+                right++;
+            }
+
+            if (right == len) {
+                if (result[left] == 'R') {
+                    for (int i = left; i < len; i++) {
+                        result[i] = 'R';
+                    }
+                }
+                break;
+            }
+
+            if ((result[left] == '.' && result[right] == 'L') || result[left] == result[right]) {
+                for (int i = left; i < right; i++) {
+                    result[i] = result[right];
+                }
+            } else if (result[left] == 'R' && result[right] == 'L') {
+                int i = left;
+                int j = right;
+
+                while (i < j) {
+                    result[i++] = 'R';
+                    result[j--] = 'L';
+                }
+            }
+
+            left = right;
+            right = left + 1;
         }
-        return sb.toString();
+        return new String(result);
     }
 }
